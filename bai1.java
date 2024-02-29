8f468477fda3a218dd3a7998883197fa855c059b
DucThien <nguyenducthienlq1@gmail.com>
2024-02-29 14:42:27 +0700

    first cm

---
+++ bai1.java
@@ -0,0 +1,18 @@
+
+public class bai1 {
+	public static void main(String[] args) {
+		
+	Private String iD;
+	
+	Private String name;
+	
+	Private String add;
+	
+	Public String getName() {
+		return name;
+	}
+	Public String get iD() {
+		return id;
+	}
+}
+}
